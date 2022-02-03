1ebc52e5ddc5da03c96d843afd3b2caf419b3178
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2022-02-03 12:19:20 +0000

    Fix: apply the homepage style classes

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -7,11 +7,11 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
-      <Link href="/ninjas"><a>See Ninja Listing</a></Link>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
+      <Link href="/ninjas"><a className={styles.btn}>See Ninja Listing</a></Link>
     </div>
   )
 }
