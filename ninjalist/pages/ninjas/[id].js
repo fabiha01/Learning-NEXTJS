3d0eafa6bbdea9a4cec7a7f893313d1ac29af1f5
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2022-02-03 15:49:50 +0000

    Fix: fetch details for each user using ID

--- ninjalist/pages/ninjas/[id].js
+++ ninjalist/pages/ninjas/[id].js
@@ -1,9 +1,40 @@
-const Details = () => {
-  return ( 
-    <div>
-      <h1>Details page</h1>
-    </div>
-   );
+// This will tell NEXT how many pages need to be made based on data
+export const getStaticPaths = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  // map data to an array of path objects with params (id)
+  const paths = data.map(ninja => {
+    return {
+      params: { id: ninja.id.toString() }
+    }
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
 }
- 
+
+// For each of those items and it will fetch them and pass them into props
+export const getStaticProps = async (context) => {
+  const id = context.params.id;
+  const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data }
+  }
+}
+const Details = ({ ninja }) => {
+  return (
+    <div>
+      <h1>{ ninja.name }</h1>
+      <p>{ ninja.email }</p>
+      <p>{ ninja.website }</p>
+      <p>{ ninja.address.city }</p>
+    </div>
+  );
+}
+
 export default Details;
