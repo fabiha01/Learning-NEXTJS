a44e05c20c1d6071c03e35eee51364f9c9fd99b5
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2022-02-03 11:54:53 +0000

    Fix: add link to the homepage

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -2,16 +2,16 @@ import Head from 'next/head'
 import Footer from '../comps/Footer';
 import Navbar from '../comps/Navbar';
 import styles from '../styles/Home.module.css'
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div>
-      <Navbar/>
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
-      <Footer />
+      <Link href="/ninjas"><a>See Ninja Listing</a></Link>
     </div>
   )
 }
