177574c2d72e4519ce794f3296f43b6a336d134a
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2022-02-03 11:36:37 +0000

    Fix: add navbar and footer components on homepage

--- ninjalist/pages/index.js
+++ ninjalist/pages/index.js
@@ -1,14 +1,17 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import Footer from '../comps/Footer';
+import Navbar from '../comps/Navbar';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
+      <Footer />
     </div>
   )
 }
