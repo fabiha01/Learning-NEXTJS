738dd5266ab444cfd10d4fbf84b39f6ef8681aa0
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2022-02-03 10:50:18 +0000

    Fix: Initial commit

---
+++ ninjalist/pages/about.js
@@ -0,0 +1,13 @@
+const  About = () => {
+  return ( 
+    <div>
+      <h1>About</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat arcu sed ligula rhoncus ornare. Pellentesque congue ullamcorper neque.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat arcu sed ligula rhoncus ornare. Pellentesque congue ullamcorper neque.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat arcu sed ligula rhoncus ornare. Pellentesque congue ullamcorper neque.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat arcu sed ligula rhoncus ornare. Pellentesque congue ullamcorper neque.</p>
+    </div>
+  );
+}
+ 
+export default About;
---
+++ ninjalist/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima soluta assumenda consequuntur, quia neque cupiditate quod voluptatibus aspernatur dolores repudiandae dignissimos maxime. Totam, ab nihil quas commodi minus odit magnam.</p>
+    </div>
+  )
+}
