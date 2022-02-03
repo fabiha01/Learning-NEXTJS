ddbf6bbcc1758678960791008b6d2e79b04ddf42
fabiha01 <43705899+fabiha01@users.noreply.github.com>
2022-02-03 12:52:47 +0000

    Fix: add meta data and title to the head

--- ninjalist/pages/about.js
+++ ninjalist/pages/about.js
@@ -1,12 +1,20 @@
+import Head from 'next/head';
+
 const  About = () => {
   return ( 
-    <div>
-      <h1>About</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat arcu sed ligula rhoncus ornare. Pellentesque congue ullamcorper neque.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat arcu sed ligula rhoncus ornare. Pellentesque congue ullamcorper neque.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat arcu sed ligula rhoncus ornare. Pellentesque congue ullamcorper neque.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat arcu sed ligula rhoncus ornare. Pellentesque congue ullamcorper neque.</p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat arcu sed ligula rhoncus ornare. Pellentesque congue ullamcorper neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat arcu sed ligula rhoncus ornare. Pellentesque congue ullamcorper neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat arcu sed ligula rhoncus ornare. Pellentesque congue ullamcorper neque.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam feugiat arcu sed ligula rhoncus ornare. Pellentesque congue ullamcorper neque.</p>
+      </div>
+    </>
   );
 }
  
