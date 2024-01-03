7b011f84e176196eb63ca3c98dc104977b0f8934
Fabiha Khatun <43705899+fabiha01@users.noreply.github.com>
2024-01-03 11:05:38 +0000

    fix: import Auth form and pass click handler as props

--- dojo-helpdesk/app/(auth)/signup/page.jsx
+++ dojo-helpdesk/app/(auth)/signup/page.jsx
@@ -1,7 +1,17 @@
+"use client"
+
+import AuthForm from "../AuthForm";
+
 export default function Signup() {
+
+  const handleSubmit = async(e, email, password) => {
+    e.preventDefault();
+    console.log('user sign up', email, password);
+  }
   return (
     <main>
       <h2 className="text-center">Sign Up</h2>
+      <AuthForm handleSubmit={handleSubmit} />
     </main>
   )
 }
